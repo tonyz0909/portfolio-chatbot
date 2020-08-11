de3addf0ade1dacbcfb006ad5bda9d14d680c952
tonyz0909 <tonyz0909@gmail.com>
2020-08-11 11:52:04 -0400

    refactoring

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./build'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
